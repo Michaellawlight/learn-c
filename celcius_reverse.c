154eccde4b8c758f773ba84663182494e738f831
Rafi Khan <zetoslab@gmail.com>
2017-07-01 15:59:01 -0600

    Change celcius_reverse
    Using constants to get rid of "magic numbers"

--- celcius_reverse.c
+++ celcius_reverse.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main()
 {
 
-  for (float current = 300; current >= 0; current -= 20) {
+  for (float current = UPPER; current >= LOWER; current -= STEP) {
     printf("%3.0f\t%6.1f\n", current, (5.0 / 9.0) * (current - 32.0));
   }
   
