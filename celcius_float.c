4eb8591cce94d70a34220bf526f07286aefbdc21
Rafi Khan <zetoslab@gmail.com>
2017-07-01 15:43:38 -0600

    Changed celcius_float
    Added a corresponding celcius to fahrenheight table. One problem
    however is that the steps do not match and so the same values are not converted.

--- celcius_float.c
+++ celcius_float.c
@@ -13,13 +13,45 @@ int main()
   printf("Fahr to Celcius: %.0fF to %.0fF\n", lower, upper);
   printf("----------------------------------\n");
 
-  while (lower <= upper) {
-    float celcius = (5.0 / 9.0) * (lower - 32.0);
+  float current = lower;
 
-    printf("%3.0f\t%6.1f\n", lower, celcius);
+  int pass = 0;
+  float lower_celcius;
+
+  float celcius;
+
+  while (current <= upper) {
+    celcius = (5.0 / 9.0) * (current- 32.0);
+
+    if (!pass) {
+      lower_celcius = celcius;
+      pass = 1;
+    }
+      
+
+    printf("%3.0f\t%6.1f\n", current, celcius);
     
-    lower += step;
+    current += step;
   }
+
+  float step_celcius = (5.0 / 9.0) * (step - 32.0);
+
+  if ( step_celcius < 0 ) {
+    step_celcius *= -1;
+  }
+
+  float upper_celcius = celcius;
+
+  printf("\n");
+
+  printf("Celcius to Fahr: %.0fF to %.0fF\n", lower, upper);
+  printf("---------------------------------------\n");
+
+  for (;lower_celcius <= upper_celcius; lower_celcius += step_celcius) {
+    printf("%6.1f\t%3.0f\n", lower_celcius, lower_celcius * (9.0 / 5.0) + 32);
+    lower_celcius += step_celcius;
+  }
+
 
   return 0;
 }
